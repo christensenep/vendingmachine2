ebebb3151f44f15b0fe2108e87c12b1f361344e2
Erik Christensen <christensenep@gmail.com>
2016-09-24 14:15:22 -0400

    Get coin diameter

--- src/main/java/com/christensenep/Coin.java
+++ src/main/java/com/christensenep/Coin.java
@@ -2,10 +2,13 @@ package com.christensenep;
 
 public class Coin {
   private final double weight;
+  private final double diameter;
 
-  public Coin(double weight) {
+  public Coin(double weight, double diameter) {
     this.weight = weight;
+    this.diameter = diameter;
   }
 
   public double getWeight() { return this.weight; }
+  public double getDiameter() { return this.diameter; }
 }
--- src/test/java/com/christensenep/CoinTest.java
+++ src/test/java/com/christensenep/CoinTest.java
@@ -7,7 +7,13 @@ import static org.junit.Assert.assertEquals;
 public class CoinTest {
   @Test
   public void getCoinWeight() {
-    Coin coin = new Coin(10.0);
+    Coin coin = new Coin(10.0, 15.0);
     assertEquals(10.0, coin.getWeight(), 0);
+  }
+
+  @Test
+  public void getCoinDiameter() {
+    Coin coin = new Coin(10.0, 15.0);
+    assertEquals(15.0, coin.getDiameter(), 0);
   }
 }
