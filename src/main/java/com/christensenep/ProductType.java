0613b4ddbbccfc33bc5ea4eb8ee4682f0e39cd2f
Erik Christensen <christensenep@gmail.com>
2016-10-01 21:51:14 -0400

    Test getting product value

--- src/main/java/com/christensenep/ProductType.java
+++ src/main/java/com/christensenep/ProductType.java
@@ -1,7 +1,15 @@
 package com.christensenep;
 
 public enum ProductType {
-  COLA,
-  CHIPS,
-  CANDY;
+  COLA(100),
+  CHIPS(50),
+  CANDY(65);
+
+  private final int value;
+
+  ProductType(int value) {
+    this.value = value;
+  }
+
+  public int getValue() { return this.value; }
 }
--- src/test/java/com/christensenep/ProductTypeTest.java
+++ src/test/java/com/christensenep/ProductTypeTest.java
@@ -2,6 +2,7 @@ package com.christensenep;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ProductTypeTest {
@@ -10,5 +11,10 @@ public class ProductTypeTest {
     assertNotNull(ProductType.COLA);
     assertNotNull(ProductType.CHIPS);
     assertNotNull(ProductType.CANDY);
+  }
+
+  @Test
+  public void getProductValue() {
+    assertNotEquals(0, ProductType.COLA.getValue());
   }
 }
