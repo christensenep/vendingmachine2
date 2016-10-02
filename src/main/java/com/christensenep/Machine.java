2f8f064666ed2518f5354cb5e0cebe9f52734d11
Erik Christensen <christensenep@gmail.com>
2016-10-01 20:37:13 -0400

    Test inserted value with quarter inserted

--- src/main/java/com/christensenep/Machine.java
+++ src/main/java/com/christensenep/Machine.java
@@ -1,7 +1,17 @@
 package com.christensenep;
 
 public class Machine {
+  private int insertedValue = 0;
+
   public int getInsertedValue() {
-    return 0;
+    return insertedValue;
+  }
+
+  public void insertCoin(Coin coin) {
+    CoinType coinType = CoinType.identifyCoin(coin, 0, 0);
+
+    if (coinType != null) {
+      this.insertedValue += coinType.getValue();
+    }
   }
 }
--- src/test/java/com/christensenep/MachineTest.java
+++ src/test/java/com/christensenep/MachineTest.java
@@ -1,13 +1,32 @@
 package com.christensenep;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class MachineTest {
+  private Machine machine;
+
+  @Before
+  public void initialize() {
+    this.machine = new Machine();
+  }
+
   @Test
   public void valueIsZeroWithNoCoinsInserted() {
-    Machine machine = new Machine();
-    assertEquals(0, machine.getInsertedValue());
+    assertEquals(0, this.machine.getInsertedValue());
+  }
+
+  @Test
+  public void valueIsCorrectWithQuarterInserted() {
+    Coin mockQuarter = mock(Coin.class);
+    when(mockQuarter.getWeight()).thenReturn(CoinType.QUARTER.getWeight());
+    when(mockQuarter.getDiameter()).thenReturn(CoinType.QUARTER.getDiameter());
+
+    assertEquals(0, this.machine.getInsertedValue());
+    this.machine.insertCoin(mockQuarter);
+    assertEquals(25, this.machine.getInsertedValue());
   }
 }
